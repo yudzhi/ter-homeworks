5a17a0c5fd386e9bd7273decc15b100500d3b3e6
yudzhi <ruthhieris@yandex.ru>
2026-07-17 15:49:57 +0300

    Задание 2 и рефакторинг кода выполнено

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,15 +10,16 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v3"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 20
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,15 +27,15 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_serial_port_enable
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
 
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -28,11 +28,66 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+# yandex_compute_image vars
+variable "vm_web_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Family of the OS image for the VM (ubuntu-2004-lts, ubuntu-2204-lts)"
+}
+
+# yandex_compute_instance vars
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "Name of the web VM instance"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "https://yandex.cloud/ru/docs/compute/concepts/vm-platforms"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default     = 2
+  description = "https://yandex.cloud/ru/docs/compute/concepts/performance-levels"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = 1
+  description = "https://yandex.cloud/ru/docs/compute/concepts/performance-levels#available-configurations"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = 20
+  description = "https://yandex.cloud/ru/docs/compute/concepts/performance-levels"
+}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  default     = true
+  description = "https://yandex.cloud/ru/docs/compute/concepts/preemptible-vm"
+}
+
+variable "vm_web_nat" {
+  type        = bool
+  default     = true
+  description = "https://yandex.cloud/ru/docs/vpc/concepts/address#public-addresses"
+}
+
+variable "vm_web_serial_port_enable" {
+  type        = number
+  default     = 1
+  description = "https://yandex.cloud/ru/docs/compute/operations/serial-console/"
+}
 
 ###ssh vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = null
+  default     = null # переопределяется в personal.auto.tfvars
   description = "ssh-keygen -t ed25519"
 }
