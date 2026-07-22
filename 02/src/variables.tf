713ccc5a0a475d2f4dd4f98cf1139b9c83adecab
yudzhi <ruthhieris@yandex.ru>
2026-07-22 15:06:15 +0300

    Выполнено задание 8

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -82,3 +82,32 @@ variable "metadata" {
     ssh-keys           = null  # переопределяется через locals
   }
 }
+
+### ==========================================
+### TEST VARIABLE (для задания 8*)
+### ==========================================
+
+variable "test" {
+  description = "Test variable with server connection details"
+  type = list(map(list(string)))
+  default = [
+    {
+      "dev1" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@62.84.124.117",
+        "10.0.1.7",
+      ]
+    },
+    {
+      "dev2" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@84.252.140.88",
+        "10.0.2.29",
+      ]
+    },
+    {
+      "prod1" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@51.250.2.101",
+        "10.0.1.30",
+      ]
+    },
+  ]
+}
